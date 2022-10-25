fa56bf1f9eac7d15f25e3fbc3dedf273976392a8
Atharva Pardeshi <atharva.exe@gmail.com>
2022-10-25 18:24:14 +0530

    feat: write basic markup

--- src/cart.tsx
+++ src/cart.tsx
@@ -1,4 +1,14 @@
-import { VStack } from "@chakra-ui/react";
+import {
+  AspectRatio,
+  Button,
+  Divider,
+  Heading,
+  HStack,
+  Image,
+  Stack,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 
 const Cart = () => {
   return (
@@ -9,7 +19,60 @@ const Cart = () => {
       spacing={10}
       alignItems="flex-start"
       bg={"gray.50"}
-    ></VStack>
+    >
+      <VStack>
+        <Heading size={"2xl"}>Your Cart</Heading>
+        <Text>
+          If price is too hard on your eyes,{" "}
+          <Button variant={"link"} colorScheme="black">
+            try changing the theme.
+          </Button>
+        </Text>
+      </VStack>
+      <HStack spacing={"6"} alignItems="center" w={"full"}>
+        <AspectRatio ratio={1} w={24}>
+          <Image src="./images/skateboard.jpg" alt="Skateboard" />
+        </AspectRatio>
+        <Stack
+          spacing={0}
+          w="full"
+          direction="row"
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <VStack w={"full"} spacing={0} alignItems="flex-start">
+            <Heading size={"md"}>Penny Board</Heading>
+            <Text color={"gray.600"}>PNYCOMP27541</Text>
+          </VStack>
+          <Heading size={"sm"} textAlign="end">
+            $119.00
+          </Heading>
+        </Stack>
+      </HStack>
+      <VStack spacing={4} alignItems="stretch" w={"full"}>
+        <HStack justifyContent={"space-between"}>
+          <Text>Subtotal</Text>
+          <Heading size={"sm"}>$119.00</Heading>
+        </HStack>
+
+        <HStack justifyContent={"space-between"}>
+          <Text>Shipping</Text>
+          <Heading size={"sm"}>$19.99</Heading>
+        </HStack>
+
+        <HStack justifyContent={"space-between"}>
+          <Text>Taxes (estimated)</Text>
+          <Heading size={"sm"}>$23.80</Heading>
+        </HStack>
+
+        <Divider />
+
+        <HStack justifyContent={"space-between"}>
+          <Text>Total</Text>
+          <Heading size={"lg"}>$162.79</Heading>
+        </HStack>
+      </VStack>
+    </VStack>
   );
 };
 
--- src/details.tsx
+++ src/details.tsx
@@ -1,14 +1,77 @@
-import { VStack } from "@chakra-ui/react";
+import {
+  Button,
+  Checkbox,
+  FormControl,
+  FormLabel,
+  GridItem,
+  Heading,
+  Input,
+  Select,
+  SimpleGrid,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 
 const Details = () => {
   return (
-    <VStack
-      w={"full"}
-      h={"full"}
-      p={10}
-      spacing={10}
-      alignItems="flex-start"
-    ></VStack>
+    <VStack w={"full"} h={"full"} p={10} spacing={10} alignItems="flex-start">
+      <VStack spacing={3} alignItems="flex-start">
+        <Heading size={"2xl"}>Your Details</Heading>
+        <Text>If you already have an account, click here to log in.</Text>
+      </VStack>
+      <SimpleGrid columns={2} columnGap={3} rowGap={6} w="full">
+        <GridItem colSpan={1}>
+          <FormControl>
+            <FormLabel>First Name</FormLabel>
+            <Input placeholder="John" />
+          </FormControl>
+        </GridItem>
+
+        <GridItem colSpan={1}>
+          <FormControl>
+            <FormLabel>Last Name</FormLabel>
+            <Input placeholder="Doe" />
+          </FormControl>
+        </GridItem>
+
+        <GridItem colSpan={2}>
+          <FormControl>
+            <FormLabel>Address</FormLabel>
+            <Input placeholder="132, My Street" />
+          </FormControl>
+        </GridItem>
+
+        <GridItem colSpan={1}>
+          <FormControl>
+            <FormLabel>City</FormLabel>
+            <Input placeholder="Kholinar" />
+          </FormControl>
+        </GridItem>
+
+        <GridItem colSpan={1}>
+          <FormControl>
+            <FormLabel>Country</FormLabel>
+            <Select>
+              <option value="usa">United States of America</option>
+              <option value="ind">India</option>
+              <option value="uae">United Arab Emirates</option>
+              <option value="uk">United Kingdom</option>
+              <option value="de">Germany</option>
+            </Select>
+          </FormControl>
+        </GridItem>
+
+        <GridItem colSpan={2}>
+          <Checkbox defaultChecked>Ship to billing address.</Checkbox>
+        </GridItem>
+
+        <GridItem colSpan={2}>
+          <Button size={"lg"} w="full">
+            Place Order
+          </Button>
+        </GridItem>
+      </SimpleGrid>
+    </VStack>
   );
 };
 

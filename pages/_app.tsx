1bd5c3b23cbc00b5712c4024810ad2d1efd3f67d
Atharva Pardeshi <atharva.exe@gmail.com>
2022-10-27 10:29:44 +0530

    feat: setup chakra to use custom theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,11 @@
 import { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 
+import theme from "../src/theme";
+
 const App = ({ Component, pageProps }) => {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
   );

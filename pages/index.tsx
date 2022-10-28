b7252ba7c9ef9ea3aed09f25ef559dd55f4c26a7
Atharva Pardeshi <atharva.exe@gmail.com>
2022-10-28 16:32:02 +0530

    feat: add Head component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,17 @@
 import { Container, Flex, VStack } from "@chakra-ui/react";
+import Head from "next/head";
 import Cart from "../src/sections/cart";
 import Details from "../src/sections/details";
 
 const IndexPage = () => (
   <Container maxW={"container.xl"} p={0}>
+    <Head>
+      <title>Penny Board | Place Order</title>
+      <meta charSet="UTF-8" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    </Head>
+
     <Flex
       h={{ base: "auto", md: "100vh" }}
       py={[0, 10, 20]}
